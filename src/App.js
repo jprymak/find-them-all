1a18d0f10392b7e68668d8a90c94057b3c1cf391
Jakub Prymak <jprymak@gmail.com>
2020-12-30 20:29:09 +0100

    feat: create SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import SearchBar from "./components/SearchBar";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <React.Fragment>
+        <SearchBar/>
+      </React.Fragment>
     </div>
   );
 }

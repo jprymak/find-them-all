ef6bb512add1b6588d15f7288eb18a25b7bb394c
Jakub Prymak <jprymak@gmail.com>
2020-12-30 20:43:53 +0100

    feat: create SearchResultBoard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import SearchBar from "./components/SearchBar";
 import React from "react";
+import SearchResultBoard from './components/SearchResultBoard';
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
         <SearchBar/>
+        <SearchResultBoard/>
       </React.Fragment>
     </div>
   );
---
+++ src/components/SearchResultBoard.js
@@ -0,0 +1,14 @@
+import './SearchResultBoard.css';
+
+import React from "react"
+
+
+function SearchResultBoard(){
+    return(
+        <div className="SearchResultBoard">
+
+        </div>
+    )
+}
+
+export default SearchResultBoard;

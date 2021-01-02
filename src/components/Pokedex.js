f122792f0372ba72f900e061ba31b0b7fb608a00
Jakub Prymak <jprymak@gmail.com>
2021-01-02 19:46:54 +0100

    feat: create Pokedex component

---
+++ src/components/Pokedex.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Pokedex(){
+    return(<div class="Pokedex">
+        <button class="Pokedex__button">Hide Pokedex</button>
+        </div>)
+}
+
+export default Pokedex;

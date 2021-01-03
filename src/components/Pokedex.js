2f46bb6ca4ace24d89d154e32b78de8e6d85fa8c
Jakub Prymak <jprymak@gmail.com>
2021-01-03 21:48:07 +0100

    feat: make pokedex content a list

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,13 +4,19 @@ import "../styles/Pokedex.css";
 function Pokedex(props) {
   const { isPokedexOpen, onPokedexButtonClick, pokedex } = props;
   return (
-    <div
-      className={`Pokedex ${isPokedexOpen ? "" : "Pokedex--hidden"}`}
-    >
-        {isPokedexOpen ? 
-        (pokedex.map((pokemon)=>{
-            return <p key={pokemon.id}>#{pokemon.id} - {pokemon.name}</p>
-        })) : ""}
+    <div className={`Pokedex ${isPokedexOpen ? "" : "Pokedex--hidden"}`}>
+      <ul>
+        {isPokedexOpen
+          ? pokedex.map((pokemon) => {
+              return (
+                <li key={pokemon.id}>
+                  #{pokemon.id} - {pokemon.name}
+                </li>
+              );
+            })
+          : ""}
+      </ul>
+
       <button onClick={onPokedexButtonClick} className="Pokedex__button">
         {isPokedexOpen ? "Hide Pokedex" : "Show Pokedex"}
       </button>

850845ac3886773f342151a6a54f8b3b5fbaf77d
Jakub Prymak <jprymak@gmail.com>
2021-01-07 21:25:21 +0100

    feat: show ability description on mouse hover

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,23 +1,38 @@
 import React from "react";
-import '../styles/PokemonCard.css';
-import { v4 as uuidv4 } from 'uuid';
+import "../styles/PokemonCard.css";
+import { v4 as uuidv4 } from "uuid";
 
-function PokemonCard(props){
-    const {imageURL, name, type, onPokemonSave, abilities} = props;
-    return(
-        <div className="pokemon-card">
-            <img alt={name} src={imageURL} className="pokemon-card__img"></img>
-            <p className="pokemon-card__name">{name}</p>
-            <p className="pokemon-card__type">{type}</p>
-            <ul className="pokemon-card__abilities">Abilities
-                {abilities.map(ability=>{
-                    return <li key={uuidv4()} className="pokemon-card__ability">{ability.name}</li>
-                })}
-
-            </ul>
-            <button onClick={()=>{onPokemonSave(name)}} className="pokemon-card__save-button" >Save in pokedex</button>
-        </div>
-    )
+function PokemonCard(props) {
+  const { imageURL, name, type, onPokemonSave, abilities} = props;
+  return (
+    <div className="pokemon-card">
+      <img alt={name} src={imageURL} className="pokemon-card__img"></img>
+      <p className="pokemon-card__name">{name}</p>
+      <p className="pokemon-card__type">{type}</p>
+      <ul className="pokemon-card__abilities">
+        Abilities
+        {abilities.map((ability) => {
+          return (
+            <li
+              key={uuidv4()}
+              className="pokemon-card__ability"
+            >
+              {ability.name}
+              <p className="pokemon-card__ability-description">{ability.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <button
+        onClick={() => {
+          onPokemonSave(name);
+        }}
+        className="pokemon-card__save-button"
+      >
+        Save in pokedex
+      </button>
+    </div>
+  );
 }
 
 export default PokemonCard;
